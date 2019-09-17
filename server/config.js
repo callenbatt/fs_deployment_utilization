9cf513f7bd80d738e91114474f69d3deea32e4a3
callenbatt@gmail.com <callenbatt@gmail.com>
2019-09-17 12:20:23 -0400

    setup initial integration

--- server/config.js
+++ server/config.js
@@ -1,3 +1,11 @@
 //defined in root config file
 var SSID = CONFIG.SSID
 var API_TOKEN = CONFIG.API_TOKEN
+
+
+//global variables
+var GET_OPTIONS = {
+    'headers' : {
+        'Authorization' : 'Bearer ' +API_TOKEN
+    }
+};
--- server/fetchStoryAllocationDays.js
+++ server/fetchStoryAllocationDays.js
@@ -3,8 +3,22 @@ function fetchStoryAllocationDays() {
 }
 
 var StoryAllocationDays = function() {
+    this.row = 2;
     this.userIds = this.getUserIds();
     this.url = this.setRequestUrl();
+
+    //fetch the first set of allocation data
+    this.fetchInit = JSON.parse(UrlFetchApp.fetch((this.url + 'page=1'), GET_OPTIONS));
+    this.pages = Math.ceil(this.fetchInit.count / 200);
+
+    //process initial fetch
+    this.processResponse(this.fetchInit, this.userIds, this.row);
+
+    //process subsequent fetchs
+    for (var i = 2; i <= this.pages; i++) {
+        var response = JSON.parse(UrlFetchApp.fetch((this.url + 'page=' + i), GET_OPTIONS));
+        this.processResponse(response, this.userIds, this.row);
+    }
     
 }
 
@@ -34,7 +48,7 @@ StoryAllocationDays.prototype.setRequestUrl = function() {
     var yearEnd = (parseInt(yearStart) + 1).toString();
     var isoEndDate = isoStartDate.replace(yearStart, yearEnd);
     var params = {
-        'per_page' : '20',
+        'per_page' : '200',
         'order' : 'date:asc', 
         'include' : 'assignment',
         'date_between' : isoStartDate + ':' + isoEndDate,
@@ -45,4 +59,32 @@ StoryAllocationDays.prototype.setRequestUrl = function() {
         paramString = paramString + paramKeys[i] + '=' + params[paramKeys[i]] + '&'
     }
     return endpoint + paramString;
+}
+
+StoryAllocationDays.prototype.processResponse = function(response, userIds, row) {
+    var output = [];
+    var responseCount = response.results.length;
+    for (var i = 0; i < responseCount; i++) {
+        var refId = response.results[i].id;
+        var allocation = response.story_allocation_days[refId];
+        var assigneeId = response.assignments[allocation.assignment_id].assignee_id;
+        if (userIds.indexOf(assigneeId) > -1) {
+            var allocationItems = [];
+            var allocationKeys = Object.keys(allocation);
+            for (var j = 0; j < allocationKeys.length; j++) {
+                allocationItems.push(allocation[allocationKeys[j]])
+            }
+            allocationItems.push(assigneeId);
+            output.push(allocationItems);
+        }
+    }
+
+    //write the formatted output to the Google Sheet
+    SpreadsheetApp.openById(SSID)
+        .getSheetByName('story_allocation_days')
+        .getRange(row, 1, output.length, output[0].length)
+        .setValues(output);
+
+    //Set the scoped row variable to pick up where the last loop left off
+    this.row = row + output.length;
 }
